6295aa1ac38e0bc326600ccac252db8f68768d9c
sroet <sanderroet@hotmail.com>
2019-07-01 18:24:33 +0200

    paremeterize tests for opt=False,True

--- dask_traj/tests/test_geometry.py
+++ dask_traj/tests/test_geometry.py
@@ -3,6 +3,7 @@ from .helpers import function_test
 import mdtraj as md
 import dask_traj as dmd
 import itertools as itt
+import pytest
 
 rel_dir = os.path.dirname(__file__)
 xtc = os.path.join(rel_dir, 'test.xtc')
@@ -18,19 +19,33 @@ class TestGeometry(object):
     def teardown(self):
         pass
 
-    def test_compute_distances(self):
-        c = list(itt.combinations(self.indices[:10], 2))
-        function_test(dask_trj=self.dask_traj,
-                      md_trj=self.md_traj,
-                      funcname='compute_distances',
-                      atom_pairs=c)
+    def slice_trajs(self, opt):
+        if opt:
+            return self.dask_traj, self.md_traj
+        else:
+            return self.dask_traj[:5], self.md_traj[:5]
 
-    def test_compute_displacements(self):
+    @pytest.mark.parametrize('opt', [True, False])
+    def test_compute_distances(self, opt):
+        dask_trj, md_trj = self.slice_trajs(opt)
+
+        c = list(itt.combinations(self.indices[:10], 2))
+        function_test(dask_trj=dask_trj,
+                      md_trj=md_trj,
+                      funcname='compute_distances',
+                      atom_pairs=c,
+                      opt=opt)
+
+    @pytest.mark.parametrize('opt', [True, False])
+    def test_compute_displacements(self, opt):
+        dask_trj, md_trj = self.slice_trajs(opt)
+
         c = list(itt.combinations(self.indices[:10], 2))
         function_test(dask_trj=self.dask_traj,
                       md_trj=self.md_traj,
                       funcname='compute_displacements',
-                      atom_pairs=c)
+                      atom_pairs=c,
+                      opt=opt)
 
     def test_compute_center_of_mass(self):
         function_test(dask_trj=self.dask_traj,
@@ -51,9 +66,13 @@ class TestGeometry(object):
                       group1=a,
                       group2=b)
 
-    def test_compute_angles(self):
+    @pytest.mark.parametrize('opt', [True, False])
+    def test_compute_angles(self, opt):
+        dask_trj, md_trj = self.slice_trajs(opt)
+
         c = list(itt.combinations(self.indices[:10], 3))
-        function_test(dask_trj=self.dask_traj,
-                      md_trj=self.md_traj,
+        function_test(dask_trj=dask_trj,
+                      md_trj=md_trj,
                       funcname='compute_angles',
-                      angle_indices=c)
+                      angle_indices=c,
+                      opt=opt)
