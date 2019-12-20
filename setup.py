f7e77c66e48f9a7d04874e9f60ee0539b31ad7a7
sroet <sanderroet@hotmail.com>
2019-12-20 18:44:18 +0100

    actually install all the subpackages as well

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup.py for dask_traj
 import os
 import subprocess
 import inspect
-from setuptools import setup
+from setuptools import setup, find_packages
 
 ####################### USER SETUP AREA #################################
 # * VERSION: base version (do not include .dev0, etc -- that's automatic)
 # * IS_RELEASE: whether this is a release
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 IS_RELEASE = False
 
 DEV_NUM = 0  # always 0: we don't do public (pypi) .dev releases
@@ -21,7 +21,7 @@ REQUIREMENTS=['future', 'mdtraj', 'dask']
 
 # PACKAGES should list any subpackages of the code. The assumption is that
 # package.subpackage is located at package/subpackage
-PACKAGES=['dask_traj', 'dask_traj.tests']
+PACKAGES=find_packages()
 
 # This DESCRIPTION is only used if a README.rst hasn't been made from the
 # markdown version
